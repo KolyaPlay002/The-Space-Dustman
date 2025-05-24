a6e270e7b4cfb9bf29f8461c915825daac8a2102
KolyaPlay002 <147727768+KolyaPlay002@users.noreply.github.com>
2025-05-24 12:13:06 +0300

    First release

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
 	t.version = "11.4" 
 	t.window.title = "The Space Dustman"
-    t.window.icon = nil
+    t.window.icon = "icon.png"
     t.window.width = 1024
     t.window.height = 576
 	t.window.resizabble = true
