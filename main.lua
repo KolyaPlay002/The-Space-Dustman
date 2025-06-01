4329e4fbd370ed7f12613c73e1d1baae85b5d8bd
KolyaPlay002 <147727768+KolyaPlay002@users.noreply.github.com>
2025-06-01 19:00:47 +0300

    Full realization of asteroids
    --Add asteroid spawn
    --Add asteroid collision
    --Add smart asteroid spawn
    --Add asteroid optimization

--- main.lua
+++ main.lua
@@ -1,30 +1,39 @@
 function love.load()
+	game_state = 0
 	hp = 200
 	bolt_tex = love.graphics.newImage("res/bolt.png")
 	ship_tex = love.graphics.newImage("res/ship.png")
+	ast_tex1 = love.graphics.newImage("res/asteroid01.png")
 	x_pos = 1024/2
 	y_pos = 576/2
 	rot = 0
 	x_sp = 0
 	y_sp = 0
 	bolt_cd = 0
+	ast_cd = 2
 	math.randomseed(os.time())
-	objects = {}
-	to_remove = nil
+	bolts = {}
+	asteroids = {}
+	to_remove_bolts = nil
+	to_remove_asts = nil
 end
 
 function love.draw()
 	love.graphics.clear(0, 0, 0) --Очищаем BG (чтобы пропадали удаленные объекты)
-	quad = love.graphics.rectangle("fill", 5, 5, hp*2, 20) --Рисуем ХП
 	--Отрисовка объектов
-	for i = 1, #objects do
-		if objects[i] ~= nil then 
-			if objects[i][1] == 1 then
-				tex = bolt_tex
-			end
-			love.graphics.draw(tex, objects[i][2], objects[i][3], 0, 1, 1, 8, 8)
+	for i = 1, #bolts do
+		if bolts[i] ~= nil then
+			tex = bolt_tex
+			love.graphics.draw(tex, bolts[i][1], bolts[i][2], 0, 1, 1, 8, 8)
 		end
 	end
+	for i = 1, #asteroids do
+		if asteroids[i] ~= nil then
+			tex = ast_tex1
+			love.graphics.draw(tex, asteroids[i][1], asteroids[i][2], 0, 1, 1, 16, 16)
+		end
+	end
+	hp_bar = love.graphics.rectangle("fill", 5, 5, hp*2, 20) --Рисуем ХП
 	ship = love.graphics.draw(ship_tex, x_pos, y_pos, r, 1, 1, 16, 16)
 end
 
@@ -32,19 +41,60 @@ function love.update(dt)
 	--База
 	r = rot * math.pi / 180
 	if bolt_cd < 1 then
-		table.insert(objects, {1, math.random(0, 1024-16), math.random(25, 576-16)})
+		table.insert(bolts, {math.random(0, 1024-16), math.random(33, 576-16)})
 		bolt_cd = 10
 		--print(bolt_cd)
 	else
 		bolt_cd = bolt_cd - 3*dt
 		--print(bolt_cd)
 	end	
+	
+	if ast_cd < 1 then --Спавн астероидов
+		storona = math.random(1, 4)
+		if storona == 1 then
+			table.insert(asteroids, {math.random(0, 1024-16), -40, math.random(-5, 5), math.random(-10, 0), 1})
+		elseif storona == 2 then
+			table.insert(asteroids, {1060, math.random(0, 576), math.random(-10, 0), math.random(-5, 5), 2})
+		elseif storona == 3 then
+			table.insert(asteroids, {math.random(0, 1024+16), 600, math.random(5, 5), math.random(0, 10), 3})
+		elseif storona == 4 then
+			table.insert(asteroids, {-40, math.random(0, 576), math.random(0, 10), math.random(-5, 5), 4})
+		end
+		ast_cd = 5
+		print("Сторона: ".. storona)
+	else
+		ast_cd = ast_cd - 3*dt
+		--print(bolt_cd)
+	end	
+	
 	if hp > 0 then
 		hp = hp-5*dt
 	end
 	x_pos = x_pos + x_sp*dt
 	y_pos = y_pos + y_sp*dt
 	
+	for i = 1, #asteroids do --Движение астероидов
+		if asteroids[i] ~= nil then
+			asteroids[i][1] = asteroids[i][1] + asteroids[i][3]*(dt*30)
+			asteroids[i][2] = asteroids[i][2] + asteroids[i][4]*(dt*30)
+		end	
+	end
+	
+	for i = 1, #asteroids do --Уничтожение лишних астероидов
+		if asteroids[i][5] == 1 and asteroids[i][2] > 600 then
+			table.remove(asteroids, i)
+			break
+		elseif asteroids[i][5] == 2 and asteroids[i][1] < -40 then
+			table.remove(asteroids, i)
+			break
+		elseif asteroids[i][5] == 3 and asteroids[i][2] < -40 then
+			table.remove(asteroids, i)
+			break
+		elseif asteroids[i][5] == 4 and asteroids[i][1] > 1060 then
+			table.remove(asteroids, i)
+			break
+		end
+	end
 	--Логика движения
 	if love.keyboard.isDown("right") then
 		x_sp = x_sp + 1
@@ -104,17 +154,25 @@ function love.update(dt)
 	
 	--Проверка коллизий
 	--print(to_remove)
-	if to_remove ~= nil then
-		table.remove(objects, to_remove)
-		to_remove = nil
+	if to_remove_bolts ~= nil then
+		table.remove(bolts, to_remove_bolts)
+		to_remove_bolts = nil
 	end
-	for i = 1, #objects do
-		
-		if objects[i][1] == 1 then
-			if objects[i][2] <= x_pos+24 and objects[i][2] >= x_pos-24 and objects[i][3] >= y_pos-24 and objects[i][3] <= y_pos+24 then
-				to_remove = i
-				hp = hp + 20
-			end
+	for i = 1, #bolts do
+		if bolts[i][1] <= x_pos+24 and bolts[i][1] >= x_pos-24 and bolts[i][2] >= y_pos-24 and bolts[i][2] <= y_pos+24 then
+			to_remove_bolts = i
+			hp = hp + 20
+		end
+	end
+	
+	if to_remove_asts ~= nil then
+		table.remove(asteroids, to_remove_asts)
+		to_remove_asts = nil
+	end
+	for i = 1, #asteroids do
+		if asteroids[i][1] <= x_pos+40 and asteroids[i][1] >= x_pos-40 and asteroids[i][2] >= y_pos-40 and asteroids[i][2] <= y_pos+40 then
+			to_remove_asts = i
+			hp = hp - 40
 		end
 	end
 end	
